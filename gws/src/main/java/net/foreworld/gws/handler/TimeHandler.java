0d9b7ff7b794e4a795f0dc14e8417ae872c6d599
unknown <3203317@qq.com>
2017-04-30 22:25:02 +0800

    haha

---
+++ gws/src/main/java/net/foreworld/gws/handler/TimeHandler.java
@@ -0,0 +1,36 @@
+package net.foreworld.gws.handler;
+
+import io.netty.channel.ChannelHandler.Sharable;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author huangxin
+ *
+ */
+@Component
+@Sharable
+public class TimeHandler extends ChannelInboundHandlerAdapter {
+
+	private static final Logger logger = LoggerFactory
+			.getLogger(TimeHandler.class);
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+		cause.printStackTrace();
+		ctx.close();
+	}
+
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) {
+
+		ctx.writeAndFlush(new TextWebSocketFrame(Long.toString(System
+				.currentTimeMillis())));
+	}
+}
--- gws/src/main/java/net/foreworld/gws/initializer/WsInitializer.java
+++ gws/src/main/java/net/foreworld/gws/initializer/WsInitializer.java
@@ -3,10 +3,17 @@ package net.foreworld.gws.initializer;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.http.HttpContentCompressor;
 import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpServerCodec;
 import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
 import io.netty.handler.stream.ChunkedWriteHandler;
+
+import javax.annotation.Resource;
+
+import net.foreworld.gws.codec.MsgCodec;
+import net.foreworld.gws.handler.EchoHandler;
+import net.foreworld.gws.handler.TimeHandler;
 
 import org.springframework.stereotype.Component;
 
@@ -18,14 +25,26 @@ import org.springframework.stereotype.Component;
 @Component
 public class WsInitializer extends ChannelInitializer<NioSocketChannel> {
 
+	@Resource(name = "timeHandler")
+	private TimeHandler timeHandler;
+
+	@Resource(name = "echoHandler")
+	private EchoHandler echoHandler;
+
+	@Resource(name = "msgCodec")
+	private MsgCodec msgCodec;
+
 	@Override
 	protected void initChannel(NioSocketChannel ch) throws Exception {
 		ChannelPipeline pipe = ch.pipeline();
 
 		pipe.addLast(new HttpServerCodec());
-		pipe.addLast(new ChunkedWriteHandler());
 		pipe.addLast(new HttpObjectAggregator(1024 * 64));
-		pipe.addLast(new WebSocketServerProtocolHandler("/ws"));
+		pipe.addLast(new ChunkedWriteHandler());
+		pipe.addLast(new HttpContentCompressor());
+		pipe.addLast(new WebSocketServerProtocolHandler("/"));
+
+		pipe.addLast(timeHandler);
 	}
 
 }
