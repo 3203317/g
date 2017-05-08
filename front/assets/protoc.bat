d2d2a3fa0f5bde7f2826f06c5094894b54d76630
unknown <3203317@qq.com>
2017-05-08 21:21:44 +0800

    haha

--- front/assets/protoc.bat
+++ front/assets/protoc.bat
@@ -1,3 +1,3 @@
 protoc.exe --java_out=..\..\gws\src\main\java .\protobuf\*
 
-protoc.exe --js_out=import_style=commonjs,binary:. .\protobuf\*
+protoc.exe --js_out=library=.\protobuf\emag,binary:. .\protobuf\*
