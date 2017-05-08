ae00b06bc84944f51abcb80eb384b30e9a764a46
3203317 <3203317@qq.com>
2017-05-08 10:05:33 +0800

    haha

--- front/assets/protoc.bat
+++ front/assets/protoc.bat
@@ -1,1 +1,3 @@
 protoc.exe --java_out=..\..\gws\src\main\java .\protobuf\*
+
+protoc.exe --js_out=import_style=commonjs,binary:. .\protobuf\*
--- logon/src/login/controllers/lobby.js
+++ logon/src/login/controllers/lobby.js
@@ -14,7 +14,7 @@ const biz = require('emag.biz');
 
 
 exports.indexUI = function(req, res, next){
-  res.render('lobby', {
+  res.render('lobby/1_0_1', {
     conf: conf,
     title: '大厅 | '+ conf.corp.name,
     data: {}
