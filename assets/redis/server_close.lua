824a828805bb030e027d2e607fc06ae0223c80b1
3203317 <3203317@qq.com>
2017-05-22 09:10:01 +0800

    haha

--- assets/redis/server_close.lua
+++ assets/redis/server_close.lua
@@ -1,10 +1,9 @@
 -- huangxin <3203317@qq.com>
---
--- redis.call('SELECT', 1);
---
--- local server_id = ARGV[1];
--- local status = ARGV[2];
---
--- redis.call('HMSET', server_id, 'connCount', 0, 'status', status);
+
+redis.call('SELECT', 1);
+
+local server_id = ARGV[1];
+
+redis.call('DEL', server_id);
 
 return 'OK';
