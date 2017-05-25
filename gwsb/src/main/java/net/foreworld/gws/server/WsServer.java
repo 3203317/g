d5533b07d2733f526b2f82e5cd6f815959b5bff4
huangxin <3203317@qq.com>
2017-05-25 17:12:45 +0800

    haha

--- gwsb/src/main/java/net/foreworld/gws/server/WsServer.java
+++ gwsb/src/main/java/net/foreworld/gws/server/WsServer.java
@@ -1,126 +1,38 @@
 package net.foreworld.gws.server;
 
-import javax.annotation.Resource;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.jms.core.JmsMessagingTemplate;
 import org.springframework.stereotype.Component;
-
-import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelOption;
-import io.netty.channel.EventLoopGroup;
-import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.nio.NioServerSocketChannel;
-import net.foreworld.gws.initializer.WsInitializer;
-import net.foreworld.gws.util.ChannelUtil;
 
 /**
  *
  * @author huangxin <3203317@qq.com>
  *
  */
-@PropertySource("classpath:activemq.properties")
 @PropertySource("classpath:server.properties")
 @Component
 public class WsServer extends Server {
 
 	private static final Logger logger = LoggerFactory.getLogger(WsServer.class);
 
-	@Value("${server.port:1234}")
-	private int port;
-
-	@Value("${server.bossThread:2}")
-	private int bossThread;
-
-	@Value("${server.workerThread:8}")
-	private int workerThread;
-
-	@Value("${server.so.backlog:1024}")
-	private int so_backlog;
-
-	@Value("${server.id}")
-	private String server_id;
-
-	@Value("${queue.front.start}")
-	private String queue_front_start;
-
-	@Value("${queue.front.stop}")
-	private String queue_front_stop;
-
-	@Resource(name = "wsInitializer")
-	private WsInitializer wsInitializer;
-
-	private ChannelFuture f;
-	private EventLoopGroup bossGroup, workerGroup;
-
-	@Resource(name = "jmsMessagingTemplate")
-	private JmsMessagingTemplate jmsMessagingTemplate;
-
 	@Override
 	public void start() {
 
-		bossGroup = new NioEventLoopGroup(bossThread);
-		workerGroup = new NioEventLoopGroup(workerThread);
-
-		ServerBootstrap b = new ServerBootstrap();
-
-		b.localAddress(port);
-		b.group(bossGroup, workerGroup);
-		b.channel(NioServerSocketChannel.class);
-
-		b.option(ChannelOption.SO_BACKLOG, so_backlog);
-		b.option(ChannelOption.SO_KEEPALIVE, true);
-		b.option(ChannelOption.TCP_NODELAY, true);
-
-		// b.handler(new LoggingHandler(LogLevel.INFO));
-
-		b.childHandler(wsInitializer);
-
-		try {
-			f = b.bind().sync();
-			if (f.isSuccess()) {
-				logger.info("ws start {}", port);
-				afterStart();
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			@Override
+			public void run() {
+				beforeShut();
+				shutdown();
 			}
-		} catch (InterruptedException e) {
-			logger.error(e.getMessage(), e);
-		} finally {
-			Runtime.getRuntime().addShutdownHook(new Thread() {
-				@Override
-				public void run() {
-					beforeShut();
-					shutdown();
-				}
-			});
-		}
+		});
 	}
 
 	@Override
 	public void shutdown() {
-		if (null != f) {
-			f.channel().close().syncUninterruptibly();
-		}
-		if (null != bossGroup) {
-			bossGroup.shutdownGracefully();
-		}
-		if (null != workerGroup) {
-			workerGroup.shutdownGracefully();
-		}
 	}
 
 	private void beforeShut() {
-		ChannelUtil.getDefault().close();
-		jmsMessagingTemplate.convertAndSend(queue_front_stop, server_id);
-		logger.info("amq stop {}", server_id);
-	}
-
-	private void afterStart() {
-		jmsMessagingTemplate.convertAndSend(queue_front_start, server_id);
-		logger.info("amq start {}", server_id);
 	}
 
 }
