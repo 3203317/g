4e74f000f8cbdf471f5de3491a2089195d23e178
huangxin <3203317@qq.com>
2017-06-01 17:49:57 +0800

    haha

---
+++ gwsb-frame/src/main/java/net/foreworld/gws/handler/User.java
@@ -0,0 +1,24 @@
+package net.foreworld.gws.handler;
+
+import org.springframework.stereotype.Component;
+
+import net.foreworld.gws.util.Action;
+
+/**
+ * 
+ * @author huangxin <3203317@qq.com>
+ *
+ */
+@Component
+public class User {
+
+	@Action(id = 95)
+	public void login() {
+		// TODO
+	}
+
+	@Action(id = 96)
+	public void logout() {
+		// TODO
+	}
+}
