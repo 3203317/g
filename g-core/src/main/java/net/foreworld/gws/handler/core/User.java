e28576efd1be04119072972f5387e939498884a5
unknown <3203317@qq.com>
2017-06-01 21:23:19 +0800

    haha

---
+++ g-core/src/main/java/net/foreworld/gws/handler/core/User.java
@@ -0,0 +1,24 @@
+package net.foreworld.gws.handler.core;
+
+import net.foreworld.gws.annotation.Action;
+
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author huangxin <3203317@qq.com>
+ *
+ */
+@Component
+public class User {
+
+	@Action(id = 95)
+	public void login() {
+		// TODO
+	}
+
+	@Action(id = 96)
+	public void logout() {
+		// TODO
+	}
+}
