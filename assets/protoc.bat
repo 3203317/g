1598635115f0045b66a02e46d9a8d4240278d366
3203317 <3203317@qq.com>
2017-06-02 11:32:01 +0800

    haha

--- assets/protoc.bat
+++ assets/protoc.bat
@@ -1,4 +1,4 @@
-del ..\front\assets\js\*.js
+del ..\front\assets\js\emag\*.js
 
 protoc.exe --java_out=..\gws-protobuf-common\src\main\java\ .\protobuf\common.proto
-protoc.exe --js_out=library=..\front\assets\js\emag_common,binary:. .\protobuf\common.proto
+protoc.exe --js_out=library=..\front\assets\js\emag\common,binary:. .\protobuf\common.proto
