feb9b9c16bd6e6223075d4026223e153e50abfee
3203317 <3203317@qq.com>
2017-06-02 14:03:31 +0800

    haha

--- assets/protoc.bat
+++ assets/protoc.bat
@@ -5,4 +5,5 @@ protoc.exe --js_out=library=..\front\assets\js\emag\common,binary:. .\protobuf\c
 
 
 
+protoc.exe --java_out=..\gws-protobuf-user\src\main\java\ .\protobuf\method.user.login.proto .\protobuf\method.user.logout.proto .\protobuf\method.user.get.proto .\protobuf\model.user.proto
 protoc.exe --js_out=library=..\front\assets\js\emag\user,binary:. .\protobuf\method.user.login.proto .\protobuf\method.user.logout.proto .\protobuf\method.user.get.proto .\protobuf\model.user.proto
