2894237c1928639e94d46a4675a128e96e1fcb86
3203317 <3203317@qq.com>
2017-06-03 08:22:14 +0800

    haha

---
+++ gws-plugin-channel/src/main/java/net/foreworld/gws/server/WsServer.java
@@ -0,0 +1,33 @@
+package net.foreworld.gws.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import net.foreworld.util.Server;
+
+/**
+ *
+ * @author huangxin <3203317@qq.com>
+ *
+ */
+@Component
+public class WsServer extends Server {
+
+	private static final Logger logger = LoggerFactory.getLogger(WsServer.class);
+
+	@Override
+	public void start() {
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			@Override
+			public void run() {
+				shutdown();
+			}
+		});
+	}
+
+	@Override
+	public void shutdown() {
+	}
+
+}
