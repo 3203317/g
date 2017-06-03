a5545723c07faaffabb558a484c6ff6cec3b3cca
unknown <3203317@qq.com>
2017-06-03 22:07:34 +0800

    haha

---
+++ gws-biz/src/main/java/net/foreworld/service/impl/RoleServiceImpl.java
@@ -0,0 +1,69 @@
+package net.foreworld.service.impl;
+
+import java.util.Date;
+import java.util.List;
+
+import net.foreworld.model.ResultMap;
+import net.foreworld.model.Role;
+import net.foreworld.service.RoleService;
+
+import org.springframework.stereotype.Service;
+
+import tk.mybatis.mapper.entity.Example;
+
+import com.github.pagehelper.PageHelper;
+
+/**
+ *
+ * @author huangxin <3203317@qq.com>
+ *
+ */
+@Service("roleService")
+public class RoleServiceImpl extends BaseService<Role> implements RoleService {
+
+	@Override
+	public int updateNotNull(Role entity) {
+		entity.setCreate_time(null);
+		return super.updateNotNull(entity);
+	}
+
+	@Override
+	public int save(Role entity) {
+		entity.setId(null);
+		entity.setCreate_time(new Date());
+		entity.setStatus(Status.START.value());
+		return super.save(entity);
+	}
+
+	@Override
+	public List<Role> findByRole(Role entity, int page, int rows) {
+		Example example = new Example(Role.class);
+		example.setOrderByClause("create_time DESC");
+
+		PageHelper.startPage(page, rows);
+		return selectByExample(example);
+	}
+
+	@Override
+	public ResultMap<Void> editInfo(Role entity) {
+		ResultMap<Void> map = new ResultMap<Void>();
+		entity.setStatus(null);
+		updateNotNull(entity);
+		map.setSuccess(true);
+		return map;
+	}
+
+	@Override
+	public ResultMap<Void> setStatus(String id, Status status) {
+		ResultMap<Void> map = new ResultMap<Void>();
+
+		Role entity = new Role();
+		entity.setId(id);
+		entity.setStatus(status.value());
+		updateNotNull(entity);
+
+		map.setSuccess(true);
+		return map;
+	}
+
+}
---
+++ gws-biz/src/test/java/net/foreworld/gws/test/BasicTest.java
@@ -0,0 +1,16 @@
+package net.foreworld.gws.test;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ *
+ * @author huangxin <3203317@qq.com>
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:applicationContext.xml")
+public class BasicTest {
+
+}
