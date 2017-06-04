8d1b14cbcd08c5a9e992a3ad1349a489874dd8fb
unknown <3203317@qq.com>
2017-06-04 14:50:56 +0800

    haha

--- assets/protoc.bat
+++ assets/protoc.bat
@@ -3,7 +3,10 @@ del ..\front\assets\js\emag\*.js
 protoc.exe --java_out=..\gws-protobuf-common\src\main\java\ .\protobuf\common.proto
 protoc.exe --js_out=library=..\front\assets\js\emag\common,binary:. .\protobuf\common.proto
 
-
-
 protoc.exe --java_out=..\gws-protobuf-user\src\main\java\ .\protobuf\method.user.login.proto .\protobuf\method.user.logout.proto .\protobuf\method.user.get.proto .\protobuf\model.user.proto
 protoc.exe --js_out=library=..\front\assets\js\emag\user,binary:. .\protobuf\method.user.login.proto .\protobuf\method.user.logout.proto .\protobuf\method.user.get.proto .\protobuf\model.user.proto
+
+protoc.exe --java_out=..\gws-protobuf-chat\src\main\java\ .\protobuf\method.chat.send.proto .\protobuf\model.chat.proto
+protoc.exe --js_out=library=..\front\assets\js\emag\chat,binary:. .\protobuf\method.chat.send.proto .\protobuf\model.chat.proto
+
+
--- gws-front/src/main/java/net/foreworld/gws/handler/TimeHandler.java
+++ gws-front/src/main/java/net/foreworld/gws/handler/TimeHandler.java
@@ -1,6 +1,14 @@
 package net.foreworld.gws.handler;
 
+import io.netty.channel.ChannelHandler.Sharable;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
 import javax.annotation.Resource;
+
+import net.foreworld.gws.protobuf.Common;
+import net.foreworld.gws.protobuf.Common.RequestProtobuf;
+import net.foreworld.gws.util.Constants;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,13 +16,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.jms.core.JmsMessagingTemplate;
 import org.springframework.stereotype.Component;
-
-import io.netty.channel.ChannelHandler.Sharable;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.SimpleChannelInboundHandler;
-import net.foreworld.gws.protobuf.Common;
-import net.foreworld.gws.protobuf.Common.RequestProtobuf;
-import net.foreworld.gws.util.Constants;
 
 /**
  *
@@ -26,7 +27,8 @@ import net.foreworld.gws.util.Constants;
 @Sharable
 public class TimeHandler extends SimpleChannelInboundHandler<RequestProtobuf> {
 
-	private static final Logger logger = LoggerFactory.getLogger(TimeHandler.class);
+	private static final Logger logger = LoggerFactory
+			.getLogger(TimeHandler.class);
 
 	@Resource(name = "jmsMessagingTemplate")
 	private JmsMessagingTemplate jmsMessagingTemplate;
@@ -35,14 +37,19 @@ public class TimeHandler extends SimpleChannelInboundHandler<RequestProtobuf> {
 	private String server_id;
 
 	@Override
-	protected void channelRead0(ChannelHandlerContext ctx, RequestProtobuf msg) throws Exception {
-		logger.info("{}:{}:{}:{}", msg.getVersion(), msg.getMethod(), msg.getSeqId(), msg.getTimestamp());
+	protected void channelRead0(ChannelHandlerContext ctx, RequestProtobuf msg)
+			throws Exception {
+		logger.info("{}:{}:{}:{}", msg.getVersion(), msg.getMethod(),
+				msg.getSeqId(), msg.getTimestamp());
 
-		Common.SenderProtobuf.Builder sender = Common.SenderProtobuf.newBuilder();
+		Common.SenderProtobuf.Builder sender = Common.SenderProtobuf
+				.newBuilder();
 		sender.setSender(server_id + "::" + ctx.channel().id().asLongText());
 		sender.setData(msg);
 
-		jmsMessagingTemplate.convertAndSend(Constants.PLUGIN + msg.getMethod(), sender.build().toByteArray());
+		jmsMessagingTemplate.convertAndSend(Constants.PLUGIN + msg.getMethod(),
+				sender.build().toByteArray());
+		ctx.flush();
 	}
 
 }
