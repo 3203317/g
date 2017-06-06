8ff941ba86262f2e572fe69eece23c9c43df2e17
huangxin <3203317@qq.com>
2017-06-06 17:15:24 +0800

    haha

---
+++ gws-biz/src/main/java/net/foreworld/model/Notice.java
@@ -0,0 +1,77 @@
+package net.foreworld.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author huangxin <3203317@qq.com>
+ *
+ */
+@Table(name = "w_notice")
+public class Notice implements Serializable {
+
+	private static final long serialVersionUID = 4034728784541327389L;
+
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(generator = "UUID")
+	private String id;
+
+	/**
+	 * 接收人
+	 */
+	private String receiver;
+
+	private String content;
+
+	private Date create_time;
+
+	private Integer status;
+
+	public String getReceiver() {
+		return receiver;
+	}
+
+	public void setReceiver(String receiver) {
+		this.receiver = receiver;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Date getCreate_time() {
+		return create_time;
+	}
+
+	public void setCreate_time(Date create_time) {
+		this.create_time = create_time;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+}
