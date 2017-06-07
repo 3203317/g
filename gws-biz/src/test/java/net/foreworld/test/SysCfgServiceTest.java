e2cc71c7c47c676c35eb7d1e2e23062b10ec89ab
huangxin <3203317@qq.com>
2017-06-07 10:48:43 +0800

    haha

--- gws-biz/src/test/java/net/foreworld/test/SysCfgServiceTest.java
+++ gws-biz/src/test/java/net/foreworld/test/SysCfgServiceTest.java
@@ -1,15 +1,18 @@
 package net.foreworld.test;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.annotation.Resource;
 import javax.transaction.Transactional;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.test.annotation.Rollback;
 
+import net.foreworld.model.ResultMap;
 import net.foreworld.model.SysCfg;
 import net.foreworld.service.SysCfgService;
 
@@ -32,6 +35,23 @@ public class SysCfgServiceTest extends BasicTest {
 	public void test_findBySysCfg() {
 		List<SysCfg> list = sysCfgService.findBySysCfg(null, 1, Integer.MAX_VALUE);
 		logger.debug("{}", list.size());
+	}
+
+	@Test
+	// 标明使用完此方法后事务不回滚, true时为回滚
+	public void test_editInfo() {
+		SysCfg entity = new SysCfg();
+
+		entity.setKey_("1");
+		entity.setValue_("2");
+		entity.setComment("3");
+		entity.setTitle("4");
+		entity.setCreate_time(new Date());
+
+		ResultMap<Void> map = sysCfgService.editInfo(entity);
+
+		Assert.assertTrue(map.getMsg(), map.getSuccess());
+
 	}
 
 }
