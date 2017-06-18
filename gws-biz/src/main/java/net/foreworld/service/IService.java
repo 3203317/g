a6fb330192e813c4f6356f2354bcd5d8ccebb56f
unknown <3203317@qq.com>
2017-06-18 16:21:54 +0800

    haha

--- gws-biz/src/main/java/net/foreworld/service/IService.java
+++ gws-biz/src/main/java/net/foreworld/service/IService.java
@@ -1,9 +1,6 @@
 package net.foreworld.service;
 
-import java.util.List;
-
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 /**
  * 
@@ -14,20 +11,4 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public interface IService<T> {
 
-	T selectByKey(Object key);
-
-	T getById(Object key);
-
-	int save(T entity);
-
-	int delete(Object key);
-
-	int updateAll(T entity);
-
-	int updateNotNull(T entity);
-
-	List<T> selectByExample(Object example);
-
-	@Transactional
-	int deleteByKeys(String keys);
 }
