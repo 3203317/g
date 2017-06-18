62bcd2d7233dd817261cf2d0944ab30912209b5a
unknown <3203317@qq.com>
2017-06-18 21:03:17 +0800

    haha

--- assets/redis/user_logout.lua
+++ assets/redis/user_logout.lua
@@ -8,8 +8,10 @@ local channel_id = ARGV[2];
 local server_id = ARGV[1];
 
 local _key = server_id ..'::'.. channel_id;
+local user_id = redis.call('GET', _key);
 
 redis.call('DEL', _key);
+redis.call('DEL', user_id);
 
 
 return 'OK';
