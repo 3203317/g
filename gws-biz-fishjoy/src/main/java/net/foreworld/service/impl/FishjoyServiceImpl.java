983c99365064f8cd698c9c42ecfad48ecdf9a6a7
huangxin <3203317@qq.com>
2017-06-22 11:22:17 +0800

    haha

--- gws-biz-fishjoy/src/main/java/net/foreworld/service/impl/FishjoyServiceImpl.java
+++ gws-biz-fishjoy/src/main/java/net/foreworld/service/impl/FishjoyServiceImpl.java
@@ -1,5 +1,6 @@
 package net.foreworld.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -16,7 +17,7 @@ import net.foreworld.service.FishjoyService;
  * @author huangxin
  *
  */
-@Service("chatService")
+@Service("fishjoyService")
 public class FishjoyServiceImpl extends BaseService implements FishjoyService {
 
 	private static final Logger logger = LoggerFactory.getLogger(FishjoyServiceImpl.class);
@@ -25,6 +26,19 @@ public class FishjoyServiceImpl extends BaseService implements FishjoyService {
 	public ResultMap<List<Receiver<Bullet>>> shot(String server_id, String channel_id, Bullet bullet) {
 
 		ResultMap<List<Receiver<Bullet>>> map = new ResultMap<List<Receiver<Bullet>>>();
+		map.setSuccess(false);
+
+		Bullet b = new Bullet();
+
+		Receiver<Bullet> rec = new Receiver<Bullet>();
+		rec.setServer_id(server_id);
+		rec.setChannel_id(channel_id);
+		rec.setData(b);
+
+		List<Receiver<Bullet>> list = new ArrayList<Receiver<Bullet>>();
+		list.add(rec);
+
+		map.setData(list);
 		map.setSuccess(true);
 		return map;
 	}
