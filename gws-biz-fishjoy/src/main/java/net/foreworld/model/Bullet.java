85b639cacd5fc1ec5affdd2df76b0c4393b8d4c8
huangxin <3203317@qq.com>
2017-06-26 08:39:08 +0800

    haha

---
+++ gws-biz-fishjoy/src/main/java/net/foreworld/model/Bullet.java
@@ -0,0 +1,71 @@
+package net.foreworld.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author huangxin <3203317@qq.com>
+ *
+ */
+public class Bullet implements Serializable {
+
+	private static final long serialVersionUID = -439989810486257425L;
+
+	private Integer level;
+
+	private Long create_time;
+	private String sender;
+
+	private Float x;
+	private Float y;
+	private Integer speed;
+
+	public Long getCreate_time() {
+		return create_time;
+	}
+
+	public void setCreate_time(Long create_time) {
+		this.create_time = create_time;
+	}
+
+	public String getSender() {
+		return sender;
+	}
+
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+
+	public Float getX() {
+		return x;
+	}
+
+	public void setX(Float x) {
+		this.x = x;
+	}
+
+	public Float getY() {
+		return y;
+	}
+
+	public void setY(Float y) {
+		this.y = y;
+	}
+
+	public Integer getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(Integer speed) {
+		this.speed = speed;
+	}
+
+	public Integer getLevel() {
+		return level;
+	}
+
+	public void setLevel(Integer level) {
+		this.level = level;
+	}
+
+}
