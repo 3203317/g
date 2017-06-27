f062bc260418e8bb3bcb806ff5eb88d6a1476298
huangxin <3203317@qq.com>
2017-06-27 08:32:53 +0800

    haha

--- gws-biz-group/src/main/java/net/foreworld/service/GroupService.java
+++ gws-biz-group/src/main/java/net/foreworld/service/GroupService.java
@@ -13,15 +13,14 @@ import net.foreworld.model.ResultMap;
 public interface GroupService extends IService {
 
 	/**
-	 * 搜索群组并加入
+	 * 搜索群组类型并加入
 	 * 
 	 * @param server_id
 	 * @param channel_id
 	 * @param group_type
 	 * @return
 	 */
-	ResultMap<List<Receiver<String>>> search(String server_id,
-			String channel_id, String group_type);
+	ResultMap<List<Receiver<String>>> search(String server_id, String channel_id, String group_type);
 
 	/**
 	 * 进入指定群组
@@ -31,20 +30,35 @@ public interface GroupService extends IService {
 	 * @param group_id
 	 * @return
 	 */
-	ResultMap<List<Receiver<String>>> entry(String server_id,
-			String channel_id, String group_id);
+	ResultMap<List<Receiver<String>>> entry(String server_id, String channel_id, String group_id);
 
+	/**
+	 * 退出所有群组
+	 * 
+	 * @param server_id
+	 * @param channel_id
+	 * @return
+	 */
 	ResultMap<List<Receiver<String>>> quit(String server_id, String channel_id);
 
 	/**
-	 * 观看指定群
+	 * 退出指定群组
 	 * 
 	 * @param server_id
 	 * @param channel_id
 	 * @param group_id
 	 * @return
 	 */
-	ResultMap<List<Receiver<String>>> visit(String server_id,
-			String channel_id, String group_id);
+	ResultMap<List<Receiver<String>>> quit(String server_id, String channel_id, String group_id);
+
+	/**
+	 * 旁观者
+	 * 
+	 * @param server_id
+	 * @param channel_id
+	 * @param group_id
+	 * @return
+	 */
+	ResultMap<List<Receiver<String>>> visit(String server_id, String channel_id, String group_id);
 
 }
