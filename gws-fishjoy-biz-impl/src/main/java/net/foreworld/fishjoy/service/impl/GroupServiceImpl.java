5812028eeac8fb3198418804a5f901c3887ba9b7
huangxin <3203317@qq.com>
2017-06-27 08:40:39 +0800

    haha

--- gws-fishjoy-biz-impl/src/main/java/net/foreworld/fishjoy/service/impl/GroupServiceImpl.java
+++ gws-fishjoy-biz-impl/src/main/java/net/foreworld/fishjoy/service/impl/GroupServiceImpl.java
@@ -2,14 +2,14 @@ package net.foreworld.fishjoy.service.impl;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
 import net.foreworld.model.Receiver;
 import net.foreworld.model.ResultMap;
 import net.foreworld.service.GroupService;
 import net.foreworld.service.impl.BaseService;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
 
 /**
  *
@@ -19,12 +19,10 @@ import org.springframework.stereotype.Service;
 @Service("groupService")
 public class GroupServiceImpl extends BaseService implements GroupService {
 
-	private static final Logger logger = LoggerFactory
-			.getLogger(GroupServiceImpl.class);
+	private static final Logger logger = LoggerFactory.getLogger(GroupServiceImpl.class);
 
 	@Override
-	public ResultMap<List<Receiver<String>>> search(String server_id,
-			String channel_id, String group_type) {
+	public ResultMap<List<Receiver<String>>> search(String server_id, String channel_id, String group_type) {
 
 		ResultMap<List<Receiver<String>>> map = new ResultMap<List<Receiver<String>>>();
 		map.setSuccess(true);
@@ -32,8 +30,7 @@ public class GroupServiceImpl extends BaseService implements GroupService {
 	}
 
 	@Override
-	public ResultMap<List<Receiver<String>>> entry(String server_id,
-			String channel_id, String group_id) {
+	public ResultMap<List<Receiver<String>>> entry(String server_id, String channel_id, String group_id) {
 
 		ResultMap<List<Receiver<String>>> map = new ResultMap<List<Receiver<String>>>();
 		map.setSuccess(true);
@@ -41,8 +38,7 @@ public class GroupServiceImpl extends BaseService implements GroupService {
 	}
 
 	@Override
-	public ResultMap<List<Receiver<String>>> quit(String server_id,
-			String channel_id) {
+	public ResultMap<List<Receiver<String>>> quit(String server_id, String channel_id) {
 
 		ResultMap<List<Receiver<String>>> map = new ResultMap<List<Receiver<String>>>();
 		map.setSuccess(false);
@@ -50,8 +46,15 @@ public class GroupServiceImpl extends BaseService implements GroupService {
 	}
 
 	@Override
-	public ResultMap<List<Receiver<String>>> visit(String server_id,
-			String channel_id, String group_id) {
+	public ResultMap<List<Receiver<String>>> visit(String server_id, String channel_id, String group_id) {
+
+		ResultMap<List<Receiver<String>>> map = new ResultMap<List<Receiver<String>>>();
+		map.setSuccess(true);
+		return map;
+	}
+
+	@Override
+	public ResultMap<List<Receiver<String>>> quit(String server_id, String channel_id, String group_id) {
 
 		ResultMap<List<Receiver<String>>> map = new ResultMap<List<Receiver<String>>>();
 		map.setSuccess(true);
