87ba93e99a8ebf420bd29720524423af4c2a10bd
root <root@g-1.(none)>
2017-07-03 13:57:37 +0800

    haha

---
+++ assets/redis/init.lua
@@ -0,0 +1,11 @@
+-- huangxin <3203317@qq.com>
+
+redis.call('SELECT', 0);
+redis.call('HMSET', 'fishjoy::group_type::1', 'total_players', '4', 'allow_view', 1, 'db', 21);
+
+
+
+redis.call('SELECT', 21);
+redis.call('SET', 'group_id::3', 'nil');
+
+return 'OK';
