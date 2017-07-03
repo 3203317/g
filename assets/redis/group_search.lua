5fcaeb16a1b861624bfd7861f98606692f7dfd80
unknown <3203317@qq.com>
2017-07-03 10:06:47 +0800

    haha

--- assets/redis/group_search.lua
+++ assets/redis/group_search.lua
@@ -10,17 +10,6 @@ local channel_id = ARGV[2];
 local game_name = ARGV[3]
 local group_type = ARGV[4];
 
-local result = nil;
-
--- // 根据群组类型获取对应的数据库
-
-redis.call('SELECT', 0);
-
--- local db = redis.call('HMGET', 'fishjoy::group_type::1', 'db');
-local db = redis.call('HMGET', game_name .. key1 ..'group_type'.. key1 .. group_type, 'db')[1];
-
-if (false == db) then return '40001'; end;
-
 -- // 根据服务器ID+通道ID获取用户ID
 
 redis.call('SELECT', 6);
@@ -29,57 +18,44 @@ local user_id = redis.call('GET', server_id .. key1 .. channel_id);
 
 if (false == user_id) then return 'invalid_channel'; end;
 
--- // 随机获取一个群组中的座位并占用该座位，然后将该座位移动到另一个库
+-- // 根据群组类型获取对应的数据库
+
+redis.call('SELECT', 0);
+
+-- local db = redis.call('HMGET', 'fishjoy::group_type::1', 'db');
+local db = redis.call('HMGET', game_name .. key1 ..'group_type'.. key1 .. group_type, 'db')[1];
+
+if (false == db) then return 'invalid_database'; end;
+
+-- // 随机获取一个群组中的座位（空闲）并占用该座位，然后将该座位移动到另一个库
 
 redis.call('SELECT', db);
 
 local ran_group_pos = redis.call('RANDOMKEY');
 
-if (false == ran_group_pos) then return '40003'; end;
+if (false == ran_group_pos) then return 'non_idle_pos'; end;
 
 redis.call('MOVE', ran_group_pos, 1 + db);
 
-
-----
+-- // 切换到另一个库，绑定座位（忙）和用户ID
 
 redis.call('SELECT', 1 + db);
 
+redis.call('SET', ran_group_pos, user_id);
 
-
-
-result = redis.call('SET', ran_group_pos, user_id);
-
-
-
-----
+-- // 修改用户信息
 
 redis.call('SELECT', 6);
 
-
 local group_id, group_pos_id = string.match(ran_group_pos, "(.*)%::(.*)");
-
 
 redis.call('HMSET', user_id, 'group_id', group_id, 'group_pos_id', group_pos_id);
 
-
-----
-
+-- // 获取当前群组成员（玩家和游客）
 
 redis.call('SELECT', 1 + db);
 
-
-
-
-
-
-
-
-return result;
-
-
-
-
-
+return 'OK';
 
 
 
