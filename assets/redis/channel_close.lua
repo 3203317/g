695b8449f42591f495f570d7a00868b1d3f71dda
root <3203317@qq.com>
2017-07-04 18:21:05 +0800

    haha

--- assets/redis/channel_close.lua
+++ assets/redis/channel_close.lua
@@ -1,15 +1,49 @@
 -- huangxin <3203317@qq.com>
 
-redis.call('SELECT', 2);
+----
+
+local db = KEYS[1];
 
 local server_id = ARGV[1];
 local channel_id = ARGV[2];
 
-local exist = redis.call('EXISTS', server_id ..':'.. channel_id);
-if 1 ~= exist then return 'OK'; end;
+----
 
-redis.call('SELECT', 1);
+redis.call('SELECT', db);
 
-redis.call('DECR', server_id ..':connCount');
+local user_id = redis.call('GET', server_id ..'::'.. channel_id);
 
-return 'OK';
+if (false == user_id) then return 'invalid_user_id'; end;
+
+--
+
+local group_id = redis.call('HMGET', user_id, 'group_id')[1];
+
+if (false == group_id) then return 'invalid_group_id'; end;
+
+local group_pos_id = redis.call('HMGET', user_id, 'group_pos_id')[1];
+local group_type = redis.call('HMGET', user_id, 'group_type')[1];
+
+redis.call('HDEL', user_id, 'group_id', 'group_pos_id', 'group_type');
+
+----
+
+redis.call('SELECT', 2 + db);
+
+redis.call('HDEL', 'pos::'.. group_type ..'::'.. group_id, group_pos_id);
+
+----
+
+redis.call('SELECT', 1 + db);
+
+redis.call('SADD', 'idle::'.. group_type, group_id ..'::'.. group_pos_id);
+
+----
+
+redis.call('SELECT', 2 + db);
+
+local result = redis.call('HGETALL', 'pos::'.. group_type ..'::'.. group_id);
+
+return result;
+
+
--- assets/redis/server_close.lua
+++ assets/redis/server_close.lua
@@ -1,9 +1,3 @@
 -- huangxin <3203317@qq.com>
 
-redis.call('SELECT', 1);
-
-local server_id = ARGV[1];
-
-redis.call('SET', server_id ..':'.. KEYS[2], ARGV[2]);
-
 return 'OK';
