6dd2675f7fdb79fa3b16619f8aa553a343355a06
unknown <3203317@qq.com>
2017-07-05 23:53:39 +0800

    haha

--- gws-front/src/main/java/net/foreworld/gws/initializer/WsNormalInitializer.java
+++ gws-front/src/main/java/net/foreworld/gws/initializer/WsNormalInitializer.java
@@ -11,12 +11,20 @@ import io.netty.handler.codec.http.websocketx.extensions.compression.WebSocketSe
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
 import io.netty.handler.stream.ChunkedWriteHandler;
+import io.netty.handler.timeout.IdleStateHandler;
+
+import java.util.concurrent.TimeUnit;
 
 import javax.annotation.Resource;
 
 import net.foreworld.gws.codec.JSONCodec;
+import net.foreworld.gws.handler.BlacklistHandler;
 import net.foreworld.gws.handler.EchoHandler;
+import net.foreworld.gws.handler.ExceptionHandler;
+import net.foreworld.gws.handler.ProtocolSafeV2Handler;
+import net.foreworld.gws.handler.TimeoutHandler;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 /**
@@ -33,17 +41,47 @@ public class WsNormalInitializer extends ChannelInitializer<NioSocketChannel> {
 	@Resource
 	private JSONCodec jSONCodec;
 
+	@Value("${server.idle.readerIdleTime:3}")
+	private int readerIdleTime;
+
+	@Value("${server.idle.writerIdleTime:7}")
+	private int writerIdleTime;
+
+	@Value("${server.idle.allIdleTime:10}")
+	private int allIdleTime;
+
+	@Resource(name = "timeoutHandler")
+	private TimeoutHandler timeoutHandler;
+
+	@Resource(name = "blacklistHandler")
+	private BlacklistHandler blacklistHandler;
+
+	@Resource(name = "exceptionHandler")
+	private ExceptionHandler exceptionHandler;
+
+	@Resource(name = "protocolSafeV2Handler")
+	private ProtocolSafeV2Handler protocolSafeV2Handler;
+
 	@Override
 	protected void initChannel(NioSocketChannel ch) throws Exception {
 		ChannelPipeline pipe = ch.pipeline();
 
 		pipe.addLast(new LoggingHandler(LogLevel.INFO));
 
+		pipe.addLast(exceptionHandler);
+		pipe.addLast(blacklistHandler);
+
+		pipe.addLast(new IdleStateHandler(readerIdleTime, writerIdleTime,
+				allIdleTime, TimeUnit.SECONDS));
+		pipe.addLast(timeoutHandler);
+
 		pipe.addLast(new HttpServerCodec());
 		pipe.addLast(new HttpObjectAggregator(1024 * 64));
 		pipe.addLast(new ChunkedWriteHandler());
 		pipe.addLast(new HttpContentCompressor());
+		pipe.addLast(protocolSafeV2Handler);
 		pipe.addLast(new WebSocketServerProtocolHandler("/", null, false));
+
 		pipe.addLast(new WebSocketServerCompressionHandler());
 
 		pipe.addLast(jSONCodec);
