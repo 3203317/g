539b2d3e6bd1b0b0a40cc3ea0d0b7f1d29f4eb57
unknown <3203317@qq.com>
2017-07-05 23:40:29 +0800

    haha

---
+++ gws-front/src/main/java/net/foreworld/gws/server/WsNormalServer.java
@@ -0,0 +1,128 @@
+package net.foreworld.gws.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import javax.annotation.Resource;
+
+import net.foreworld.gws.initializer.WsNormalInitializer;
+import net.foreworld.gws.util.ChannelUtil;
+import net.foreworld.util.Server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jms.core.JmsMessagingTemplate;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author huangxin <3203317@qq.com>
+ *
+ */
+@PropertySource("classpath:activemq.properties")
+@Component
+public class WsNormalServer extends Server {
+
+	private static final Logger logger = LoggerFactory
+			.getLogger(WsNormalServer.class);
+
+	@Value("${server.port:1234}")
+	private int port;
+
+	@Value("${server.bossThread:2}")
+	private int bossThread;
+
+	@Value("${server.workerThread:8}")
+	private int workerThread;
+
+	@Value("${server.so.backlog:1024}")
+	private int so_backlog;
+
+	@Value("${server.id}")
+	private String server_id;
+
+	@Value("${queue.front.start}")
+	private String queue_front_start;
+
+	@Value("${queue.front.stop}")
+	private String queue_front_stop;
+
+	@Resource(name = "wsNormalInitializer")
+	private WsNormalInitializer wsNormalInitializer;
+
+	private ChannelFuture f;
+	private EventLoopGroup bossGroup, workerGroup;
+
+	@Resource(name = "jmsMessagingTemplate")
+	private JmsMessagingTemplate jmsMessagingTemplate;
+
+	@Override
+	public void start() {
+
+		bossGroup = new NioEventLoopGroup(bossThread);
+		workerGroup = new NioEventLoopGroup(workerThread);
+
+		ServerBootstrap b = new ServerBootstrap();
+
+		b.localAddress(port);
+		b.group(bossGroup, workerGroup);
+		b.channel(NioServerSocketChannel.class);
+
+		b.option(ChannelOption.SO_BACKLOG, so_backlog);
+		b.option(ChannelOption.SO_KEEPALIVE, true);
+		b.option(ChannelOption.TCP_NODELAY, true);
+
+		// b.handler(new LoggingHandler(LogLevel.INFO));
+
+		b.childHandler(wsNormalInitializer);
+
+		try {
+			f = b.bind().sync();
+			if (f.isSuccess()) {
+				logger.info("ws start {}", port);
+				afterStart();
+			}
+		} catch (InterruptedException e) {
+			logger.error(e.getMessage(), e);
+		} finally {
+			Runtime.getRuntime().addShutdownHook(new Thread() {
+				@Override
+				public void run() {
+					beforeShut();
+					shutdown();
+				}
+			});
+		}
+	}
+
+	@Override
+	public void shutdown() {
+		if (null != f) {
+			f.channel().close().syncUninterruptibly();
+		}
+		if (null != bossGroup) {
+			bossGroup.shutdownGracefully();
+		}
+		if (null != workerGroup) {
+			workerGroup.shutdownGracefully();
+		}
+	}
+
+	private void beforeShut() {
+		ChannelUtil.getDefault().close();
+		jmsMessagingTemplate.convertAndSend(queue_front_stop, server_id);
+		logger.info("front amq stop: {}", server_id);
+	}
+
+	private void afterStart() {
+		jmsMessagingTemplate.convertAndSend(queue_front_start, server_id);
+		logger.info("front amq start: {}", server_id);
+	}
+
+}
--- logon/src/login/controllers/fishjoy.js
+++ logon/src/login/controllers/fishjoy.js
@@ -14,7 +14,7 @@ const biz = require('emag.biz');
 
 
 exports.indexUI = function(req, res, next){
-  res.render('fishjoy/1_0_2', {
+  res.render('fishjoy/1_0_3', {
     conf: conf,
     title: '捕鱼大厅 | '+ conf.corp.name,
     data: {}
