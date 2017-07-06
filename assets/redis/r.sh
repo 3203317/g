ff318acceddb485e826b5c4991a6ea05baaf4ace
root <root@g-1.(none)>
2017-07-06 21:37:08 +0800

    haha

--- assets/redis/r.sh
+++ assets/redis/r.sh
@@ -7,3 +7,11 @@
 /root/my/redis/redis-3.2.6/src/redis-cli -a 123456 script load "$(cat /root/my/git/3203317/g/assets/redis/channel_close.lua)"
 /root/my/redis/redis-3.2.6/src/redis-cli -a 123456 script load "$(cat /root/my/git/3203317/g/assets/redis/group_search.lua)"
 /root/my/redis/redis-3.2.6/src/redis-cli -a 123456 script load "$(cat /root/my/git/3203317/g/assets/redis/group_quit.lua)"
+
+
+
+/root/my/redis/redis-3.2.6/src/redis-cli -a 123456 --eval /root/my/git/3203317/g/assets/redis/init.lua 1 ,
+
+
+
+
