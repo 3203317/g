f7aaf68bd98a22cd834c7d0846ace195d0cf7b00
root <3203317@qq.com>
2017-07-11 09:22:26 +0800

    haha

--- assets/redis/token.lua
+++ assets/redis/token.lua
@@ -6,6 +6,7 @@ local code = ARGV[1];
 local server_id = ARGV[2];
 local channel_id = ARGV[3];
 -- local seconds = ARGV[4];
+local open_time = ARGV[4];
 
 -- 
 
@@ -38,12 +39,11 @@ end;
 
 -- 
 
-redis.call('HMSET', 'prop::'.. user_id, 'client_id', client_id, KEYS[2], server_id, KEYS[3], channel_id, 'scope', '');
+redis.call('HMSET', 'prop::'.. user_id, 'client_id', client_id, KEYS[2], server_id, KEYS[3], channel_id, 'scope', '', 'open_time', open_time);
 -- redis.call('EXPIRE', 'prop::'.. user_id, seconds);
 
-redis.call('SET', server_id ..'::'.. channel_id, user_id);
+redis.call('HMSET', server_id ..'::'.. channel_id, 'user_id', user_id, 'client_id', client_id, 'scope', '', 'open_time', open_time);
 -- redis.call('EXPIRE', server_id ..'::'.. channel_id, seconds);
 
--- 
-
 return result;
+
