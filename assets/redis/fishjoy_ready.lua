7db2f1d18b7b806ae84d860c656d96ccfdb421d2
3203317 <3203317@qq.com>
2017-07-11 16:34:37 +0800

    haha

--- assets/redis/fishjoy_ready.lua
+++ assets/redis/fishjoy_ready.lua
@@ -7,6 +7,7 @@ local back_id = KEYS[2];
 
 local server_id = ARGV[1];
 local channel_id = ARGV[2];
+local open_time = ARGV[3];
 
 -- 
 
@@ -30,11 +31,11 @@ local group_pos_id = redis.call('HGET', 'prop::'.. user_id, 'group_pos_id');
 
 redis.call('SELECT', 1 + db);
 
-local group_type = redis.call('HGET', 'prop::'.. group_id, 'type');
+local group_type = redis.call('HGET', 'prop::group::'.. group_id, 'type');
 
 -- 
 
-local s = redis.call('HGET', 'pos::'.. group_type ..'::'.. group_id, group_pos_id);
+local s = redis.call('HGET', 'pos::group::'.. group_type ..'::'.. group_id, group_pos_id);
 
 if (false == s) then return 'invalid_group_pos_id'; end;
 
@@ -44,20 +45,62 @@ local b, hand = string.match(s, '(.*)%::(.*)');
 
 if (b ~= user_id) then return 'invalid_user_id'; end;
 
--- 
-
-redis.call('HSET', 'pos::'.. group_type ..'::'.. group_id, group_pos_id, user_id ..'::1');
+if (1 == tonumber(hand)) then return 'already_raise_hand' end;
 
 -- 
 
-local sb = redis.call('HGET', 'prop::'.. group_id, 'back_id');
+redis.call('HSET', 'pos::group::'.. group_type ..'::'.. group_id, group_pos_id, user_id ..'::1');
 
-if (sb) then back_id = sb; end;
+-- 群组对应的后置服务器
+
+local sb = redis.call('HGET', 'prop::group::'.. group_id, 'back_id');
+
+if (false == sb) then
+
+  redis.call('HMSET', 'prop::group::'.. group_id, 'back_id', back_id, 'open_time', open_time);
+else
+
+  -- 后置服务器的启动时间
+
+  local x = redis.call('HGET', 'prop::back::'.. sb, 'open_time');
+
+  if (false == x) then
+
+    redis.call('HMSET', 'prop::group::'.. group_id, 'back_id', back_id, 'open_time', open_time);
+  else
+
+    local y = redis.call('HGET', 'prop::group::'.. group_id, 'open_time');
+
+    if (tonumber(x) < tonumber(y)) then
+
+      back_id = sb;
+    else
+
+      redis.call('HMSET', 'prop::group::'.. group_id, 'back_id', back_id, 'open_time', open_time);
+    end;
+
+  end;
+
+end;
 
 -- 
 
-redis.call('HSET', 'prop::'.. group_id, 'back_id', back_id);
+local capacity = redis.call('HGET', 'prop::group::'.. group_id, 'capacity');
 
-local capacity = redis.call('HGET', 'prop::'.. group_id, 'capacity');
+redis.call('SELECT', db);
 
-return group_type ..'::'.. group_id ..'::'.. capacity;
+local arr = {};
+
+for i=2, #hash_val, 2 do
+  local u = string.match(hash_val[i], '(.*)%::(.*)');
+
+  table.insert(arr, redis.call('HGET', 'prop::'.. u, 'server_id'));
+  table.insert(arr, redis.call('HGET', 'prop::'.. u, 'channel_id'));
+end;
+
+local result = {};
+
+table.insert(result, arr);
+table.insert(result, back_id ..'::'.. group_id ..'::'.. capacity);
+
+return result;
