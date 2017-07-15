a798399eddafe061f9ca08264fec32459f61fdcc
unknown <3203317@qq.com>
2017-07-15 23:50:35 +0800

    haha

--- logon/src/biz/fishjoy.js
+++ logon/src/biz/fishjoy.js
@@ -26,149 +26,149 @@ const fishpondPool = require('emag.model').fishpondPool;
 const biz = require('emag.biz');
 
 (() => {
-    var p1 = new Promise((resolve, reject) => {
-      ajax(http.request, {
-        host: '127.0.0.1',
-        port: 80,
-        path: '/assets/fish.trail.json',
-        method: 'GET',
-      }, null, null).then(html => {
-        resolve(JSON.parse(html));
-      }).catch(reject);
-    });
+  var p1 = new Promise((resolve, reject) => {
+    ajax(http.request, {
+      host: conf.app.resHost,
+      port: 80,
+      path: '/assets/fish.trail.json',
+      method: 'GET',
+    }, null, null).then(html => {
+      resolve(JSON.parse(html));
+    }).catch(reject);
+  });
 
-    var p2 = new Promise((resolve, reject) => {
-      ajax(http.request, {
-        host: '127.0.0.1',
-        port: 80,
-        path: '/assets/fish.type.json',
-        method: 'GET',
-      }, null, null).then(html => {
-        resolve(JSON.parse(html));
-      }).catch(reject);
-    });
+  var p2 = new Promise((resolve, reject) => {
+    ajax(http.request, {
+      host: conf.app.resHost,
+      port: 80,
+      path: '/assets/fish.type.json',
+      method: 'GET',
+    }, null, null).then(html => {
+      resolve(JSON.parse(html));
+    }).catch(reject);
+  });
 
-    Promise.all([p1, p2]).then(values => {
+  Promise.all([p1, p2]).then(values => {
 
-      function init(doc, cb){
-        console.log('[INFO ] fishjoy ready init');
+    function init(doc, cb){
+      console.log('[INFO ] fishjoy ready init');
 
-        if(!_.isArray(doc)) return;
+      if(!_.isArray(doc)) return;
 
-        var sb = doc[1].split('::');
+      var sb = doc[1].split('::');
 
-        // 如果不在同一台服务器
-        if(conf.app.id !== sb[2]) return;
+      // 如果不在同一台服务器
+      if(conf.app.id !== sb[2]) return;
 
-        var opts = {
-          id: sb[0],
-          type: sb[1],
-          capacity: sb[3],
-          fishType: values[1].data,
-          fishTrail: values[0].data,
-        };
-
-        var fishpond = fishpondPool.get(opts.id);
-        if(fishpond) return;
-
-        fishpond = fishpondPool.create(opts);
-
-        function scene1(fishpond, callback){
-          var i = 9;
-
-          (function schedule(){
-
-            var timeout = setTimeout(function(){
-              clearTimeout(timeout);
-              i--;
-
-              callback((err, doc) => {
-                if(err){
-                  fishpondPool.release(fishpond.id);
-                  return cb(err);
-                }
-
-                if('invalid_group_id' === doc){
-                  return fishpondPool.release(fishpond.id);
-                }
-
-                if(0 === doc.length){
-                  return fishpondPool.release(fishpond.id);
-                }
-
-                var s = [];
-                s.push(doc)
-                s.push(fishpond.refresh());
-
-                cb(null, s);
-
-                console.log('scene1: %j', doc);
-
-                if(0 === i) return scene2(fishpond, callback);
-                schedule();
-              });
-
-            }, 300);
-          }());
-        }
-
-        function scene2(fishpond, callback){
-          var i = 6;
-
-          (function schedule(){
-
-            var timeout = setTimeout(function(){
-              clearTimeout(timeout);
-              i--;
-
-              callback((err, doc) => {
-                if(err){
-                  fishpondPool.release(fishpond.id);
-                  return cb(err);
-                }
-
-                if('invalid_group_id' === doc){
-                  return fishpondPool.release(fishpond.id);
-                }
-
-                if(0 === doc.length){
-                  return fishpondPool.release(fishpond.id);
-                }
-
-                var s = [];
-                s.push(doc)
-                s.push(fishpond.refresh());
-
-                cb(null, s);
-
-                console.log('scene2: %j', doc);
-
-                if(0 === i) return scene1(fishpond, callback);
-                schedule();
-              });
-
-            }, 300);
-          }());
-        }
-
-        scene1(fishpond, biz.group.readyUsers.bind(null, fishpond.id));
-      }
-
-      const numkeys = 3;
-      const sha1 = '70ee764d70d6666f1704a3ba7b6697fa27fbd5b0';
-
-      exports.ready = function(server_id, channel_id, cb1, cb2){
-
-        redis.evalsha(sha1, numkeys, conf.redis.database, server_id, channel_id, conf.app.id, (new Date().getTime()), (err, doc) => {
-          if(err) return cb1(err);
-          cb1(null, doc);
-          init(doc, cb2);
-        });
+      var opts = {
+        id: sb[0],
+        type: sb[1],
+        capacity: sb[3],
+        fishType: values[1].data,
+        fishTrail: values[0].data,
       };
 
-    }).catch(function (err){
-      console.error('[ERROR] %s', err);
-    });
+      var fishpond = fishpondPool.get(opts.id);
+      if(fishpond) return;
+
+      fishpond = fishpondPool.create(opts);
+
+      function scene1(fishpond, callback){
+        var i = 9;
+
+        (function schedule(){
+
+          var timeout = setTimeout(function(){
+            clearTimeout(timeout);
+            i--;
+
+            callback((err, doc) => {
+              if(err){
+                fishpondPool.release(fishpond.id);
+                return cb(err);
+              }
+
+              if('invalid_group_id' === doc){
+                return fishpondPool.release(fishpond.id);
+              }
+
+              if(0 === doc.length){
+                return fishpondPool.release(fishpond.id);
+              }
+
+              var s = [];
+              s.push(doc)
+              s.push(fishpond.refresh());
+
+              cb(null, s);
+
+              console.log('scene1: %j', doc);
+
+              if(0 === i) return scene2(fishpond, callback);
+              schedule();
+            });
+
+          }, 300);
+        }());
+      }
+
+      function scene2(fishpond, callback){
+        var i = 6;
+
+        (function schedule(){
+
+          var timeout = setTimeout(function(){
+            clearTimeout(timeout);
+            i--;
+
+            callback((err, doc) => {
+              if(err){
+                fishpondPool.release(fishpond.id);
+                return cb(err);
+              }
+
+              if('invalid_group_id' === doc){
+                return fishpondPool.release(fishpond.id);
+              }
+
+              if(0 === doc.length){
+                return fishpondPool.release(fishpond.id);
+              }
+
+              var s = [];
+              s.push(doc)
+              s.push(fishpond.refresh());
+
+              cb(null, s);
+
+              console.log('scene2: %j', doc);
+
+              if(0 === i) return scene1(fishpond, callback);
+              schedule();
+            });
+
+          }, 300);
+        }());
+      }
+
+      scene1(fishpond, biz.group.readyUsers.bind(null, fishpond.id));
+    }
+
+    const numkeys = 3;
+    const sha1 = '70ee764d70d6666f1704a3ba7b6697fa27fbd5b0';
+
+    exports.ready = function(server_id, channel_id, cb1, cb2){
+
+      redis.evalsha(sha1, numkeys, conf.redis.database, server_id, channel_id, conf.app.id, (new Date().getTime()), (err, doc) => {
+        if(err) return cb1(err);
+        cb1(null, doc);
+        init(doc, cb2);
+      });
+    };
+
+  }).catch(function (err){
+    console.error('[ERROR] resHost: %s', err);
+  });
 
 })();
 
