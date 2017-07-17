ca8bebcfd5edd10882f2c5dd68de84b8d757a7c7
3203317 <3203317@qq.com>
2017-07-17 11:33:58 +0800

    haha

--- assets/redis/fishjoy_bullet.lua
+++ assets/redis/fishjoy_bullet.lua
@@ -13,17 +13,10 @@ local user_id = redis.call('GET', server_id ..'::'.. channel_id);
 
 if (false == user_id) then return 'invalid_user_id'; end;
 
--- 不在任何群组
-
-local group_id = redis.call('HGET', 'prop::'.. user_id, 'group_id');
-
-if (false == group_id) then return 'invalid_group_id'; end;
-
 -- 
 
 redis.call('SELECT', 1 + db);
 
-local result = redis.call('HGETALL', 'prop::bullet::'.. bullet_id);
+local result = redis.call('HGETALL', 'prop::bullet::'.. user_id ..'::'.. bullet_id);
 
 return result;
-
