cb3422daa7ae2bb319f7d8c63ed4eb250fa9e0e8
3203317 <3203317@qq.com>
2017-07-18 08:32:30 +0800

    haha

--- assets/redis/authorize.lua
+++ assets/redis/authorize.lua
@@ -16,18 +16,6 @@ if (code) then return code; end;
       code         = KEYS[4];
 
 local seconds      = ARGV[1];
-local extend_data  = ARGV[2];
-local score        = ARGV[3];
-local tool_1       = ARGV[4];
-local tool_2       = ARGV[5];
-local tool_3       = ARGV[6];
-local tool_4       = ARGV[7];
-local tool_5       = ARGV[8];
-local tool_6       = ARGV[9];
-local tool_7       = ARGV[10];
-local tool_8       = ARGV[11];
-local tool_9       = ARGV[12];
-local bullet_level = ARGV[13];
 
 redis.call('SET', _key, code);
 redis.call('EXPIRE', _key, seconds);
@@ -40,21 +28,20 @@ redis.call('EXPIRE', _key, seconds);
   }
 }
 --]]
-redis.call('HMSET', code, 'client_id', client_id, 'user_id', user_id, 'extend_data', extend_data);
-
-redis.call('HSET', code, 'score', score);
-
-redis.call('HSET', code, 'tool_1', tool_1);
-redis.call('HSET', code, 'tool_2', tool_2);
-redis.call('HSET', code, 'tool_3', tool_3);
-redis.call('HSET', code, 'tool_4', tool_4);
-redis.call('HSET', code, 'tool_5', tool_5);
-redis.call('HSET', code, 'tool_6', tool_6);
-redis.call('HSET', code, 'tool_7', tool_7);
-redis.call('HSET', code, 'tool_8', tool_8);
-redis.call('HSET', code, 'tool_9', tool_9);
-
-redis.call('HSET', code, 'bullet_level', bullet_level);
+redis.call('HMSET', code, 'client_id',    client_id,
+                          'user_id',      user_id,
+                          'extend_data',  ARGV[2],
+                          'score',        ARGV[3],
+                          'tool_1',       ARGV[4],
+                          'tool_2',       ARGV[5],
+                          'tool_3',       ARGV[6],
+                          'tool_4',       ARGV[7],
+                          'tool_5',       ARGV[8],
+                          'tool_6',       ARGV[9],
+                          'tool_7',       ARGV[10],
+                          'tool_8',       ARGV[11],
+                          'tool_9',       ARGV[12],
+                          'bullet_level', ARGV[13]);
 
 redis.call('EXPIRE', code, seconds);
 
