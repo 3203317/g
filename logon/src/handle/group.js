2878fecc9972c3d4e10b703680965b39e703ca0f
3203317 <3203317@qq.com>
2017-07-22 11:15:08 +0800

    haha

--- logon/src/handle/group.js
+++ logon/src/handle/group.js
@@ -22,6 +22,40 @@ exports.search = function(client, msg){
   this._quit(client, data.serverId, data.channelId, data.seqId, function (err){
     if(err) return logger.error('group quit:', err);
     logger.info('group quit: %j', data);
+
+    biz.group.search(data.serverId, data.channelId, data.data, function (err, doc){
+      if(err) return logger.error('group search:', err);
+
+      if(_.isArray(doc)){
+
+        var arr1 = doc[0];
+        if(!arr1) return;
+
+        var result = {
+          method: 3002,
+          seqId: data.seqId,
+          data: doc[1],
+        };
+
+        return ((function(){
+
+          for(let i=0, j=arr1.length; i<j; i++){
+            let s = arr1[i];
+            result.receiver = arr1[++i];
+
+            if(!s)               continue;
+            if(!result.receiver) continue;
+
+            client.send('/queue/back.send.v2.'+ s, { priority: 9 }, JSON.stringify(result));
+          }
+        })());
+      }
+
+      switch(doc){
+        case 'invalid_user_id':
+          return client.send('/queue/front.force.v2.'+ server_id, { priority: 9 }, channel_id);
+      }
+    });
   });
 };
 
@@ -72,7 +106,7 @@ exports._quit = function(client, server_id, channel_id, seq_id, cb){
     switch(doc){
       case 'OK': return cb();
       case 'invalid_user_id':
-        client.send('/queue/front.force.v2.'+ server_id, { priority: 9 }, channel_id);
+        return client.send('/queue/front.force.v2.'+ server_id, { priority: 9 }, channel_id);
     }
   });
 };
