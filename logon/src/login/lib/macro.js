f77b2e8ddbbc891fd22342af59d6d42851dab957
3203317 <3203317@qq.com>
2017-07-27 09:44:43 +0800

    haha

--- logon/src/login/lib/macro.js
+++ logon/src/login/lib/macro.js
@@ -9,6 +9,28 @@ const fs = require('fs'),
       velocity = require('velocityjs'),
       cwd = process.cwd();
 
+const utils = require('speedt-utils').utils;
+
+function format(date, format){
+  date = date || new Date;
+  format = format || 'hh:mm:ss.S';
+  var o = {
+    'Y+': date.getFullYear(),
+    'M+':  utils.padLeft(date.getMonth() + 1, '0', 2),    // month
+    'd+':  utils.padLeft(date.getDate(), '0', 2),         // day
+    'h+':  utils.padLeft(date.getHours(), '0', 2),        // hour
+    'm+':  utils.padLeft(date.getMinutes(), '0', 2),      // minute
+    's+':  utils.padLeft(date.getSeconds(), '0', 2),      // second
+     'S': utils.padRight(date.getMilliseconds(), '0', 3)  // millisecond
+  }
+  for(var k in o){
+    if(new RegExp('('+ k +')').test(format)){
+      format = format.replace(RegExp.$1, o[k]);
+    }
+  }
+  return format;
+};
+
 module.exports = {
   parse: function(file){
     var tpl = fs.readFileSync(require('path').join(cwd, 'views', file)).toString();
@@ -17,6 +39,10 @@ module.exports = {
   include: function(file){
     var tpl = fs.readFileSync(require('path').join(cwd, 'views', file)).toString();
     return tpl;
+  },
+  formatDate: function(t){
+    return format(t, 'YY-MM-dd hh:mm:ss');
+    // return format(t, 'YY-MM-dd hh:mm:ss.S');
   },
   toHtml: s => {
     return velocity.render(s);
